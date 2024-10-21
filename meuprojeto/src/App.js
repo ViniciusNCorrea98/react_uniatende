11b264977618cdcfb26f64d82d444d0873cd1536
Administrador <viniciuscorrea417@gmail.com>
2024-10-21 17:36:10 -0300

    first commit- basic structed

---
+++ meuprojeto/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import App1 from '../src/componentes/component1';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Worldddddd!</h1>
+      <App1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ meuprojeto/src/componentes/component1.js
@@ -0,0 +1,11 @@
+
+
+function App1() {
+  return (
+    <div className="">
+      <h1>OLA GALERA</h1>
+    </div>
+  );
+}
+
+export default App1;
